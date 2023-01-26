366b0d7213466012bae74a1dfd1df36a064aa198
Jack Lacey-Rogers <jacklaceyrogers6@gmail.com>
2023-01-26 14:35:42 -0500

    Initial Commit

---
+++ Story-PitchPerfect/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Story-PitchPerfect
+//
+//  Created by jack on 2023-01-24.
+//
+
+import UIKit
+import AVFoundation
+
+var audioRecorder: AVAudioRecorder!
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stopRecordingButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view appeared")
+    }
+    
+    @IBAction func recordAudio(_ sender: AnyObject) {
+        recordingLabel.text = "Recording in progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
+
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
+
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
+
+    
+    @IBAction func stopRecording(_ sender: Any){
+    recordButton.isEnabled = true
+    stopRecordingButton.isEnabled = false
+    recordingLabel.text = "Tap to Record"
+    audioRecorder.stop()
+    let audioSession = AVAudioSession.sharedInstance()
+    try! audioSession.setActive(false)
+    }
+}
+
+
+
